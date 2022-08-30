dc8dce36848ab72a7c9579765d6dc1b3fb545bff
freshmea <freshmea@naver.com>
2022-08-30 16:36:56 +0900

    add 2room 3room 4room

--- ChoiSugil/command.c
+++ ChoiSugil/command.c
@@ -12,7 +12,7 @@ void sCommand(char *buf)
 
 	// person.dat 사람 데이터 읽기.
 	frperson = fopen("person.dat", "r");
-	if (fin == NULL){
+	if (frperson == NULL){
 		fprintf(stdout, "can't open read person.dat file\n");
 		exit(1);
 	}
@@ -31,27 +31,66 @@ void sCommand(char *buf)
 	fclose(fin);
 
 
-	printf("sCommand excution! : %s\n", buf);
 	if (!strcmp(buf, "1personIn" ))
-	{
 		printf("1personIn : %d\n", ++p1.room1);
-	}
+	if (!strcmp(buf, "2personIn" ))
+		printf("2personIn : %d\n", ++p1.room2);
+	if (!strcmp(buf, "3personIn" ))
+		printf("3personIn : %d\n", ++p1.room3);
+	if (!strcmp(buf, "4personIn" ))
+		printf("4personIn : %d\n", ++p1.room4);
 
 	if (!strcmp(buf, "1personOut" ))
 	{
 		if (p1.room1 == 0) printf("there is no person in room1 : %d\n", p1.room1);
 		else {
 			printf("there is %d person.\n", --p1.room1);
-			if (p1.room1 == 0) printf("turn off Light\n");
+			if (p1.room1 == 0) {
+				printf("turn off Light\n");
+				strcpy(buf, "1turnOff");
+			}
+		}
+	}
+	if (!strcmp(buf, "2personOut" ))
+	{
+		if (p1.room2 == 0) printf("there is no person in room2 : %d\n", p1.room2);
+		else {
+			printf("there is %d person.\n", --p1.room2);
+			if (p1.room2 == 0) {
+				printf("turn off Light\n");
+				strcpy(buf, "2turnOff");
+			}
+		}
+	}
+	if (!strcmp(buf, "3personOut" ))
+	{
+		if (p1.room3 == 0) printf("there is no person in room1 : %d\n", p1.room3);
+		else {
+			printf("there is %d person.\n", --p1.room3);
+			if (p1.room3 == 0) {
+				printf("turn off Light\n");
+				strcpy(buf, "3turnOff");
+			}
+		}
+	}
+	if (!strcmp(buf, "4personOut" ))
+	{
+		if (p1.room4 == 0) printf("there is no person in room1 : %d\n", p1.room4);
+		else {
+			printf("there is %d person.\n", --p1.room4);
+			if (p1.room4 == 0) {
+				printf("turn off Light\n");
+				strcpy(buf, "4turnOff");
+			}
 		}
 	}
 
 	// person.dat 사람 데이터 쓰기.
 	fwperson = fopen("person.dat", "w");
-	if (fin == NULL){
+	if (fwperson == NULL){
 		fprintf(stdout, "can't open write person.dat file\n");
 		exit(1);
 	}
-	fprintf(fwperson, "%d %d %d %d", p1.room1, p1.room2,p1.room3,p1.room4);
+	fprintf(fwperson, "%d %d %d %d", p1.room1, p1.room2, p1.room3, p1.room4);
 	fclose(fwperson);
 }
